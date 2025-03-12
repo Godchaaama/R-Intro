50692c887764cf760e1265e2c425761536c237b4
Tung Le <lenhattung@gmail.com>
2025-03-12 12:49:00 +0700

    ud
    ud

---
+++ Lab-01/24-Condition_Loops.R
@@ -0,0 +1,29 @@
+# Vi du ve cau lenh dieu kien
+
+logicalVector <- c(T, F, T, T, F)
+counter<-0;
+for (v in logicalVector) {
+  if(v==T){
+    counter <-counter+1;
+  }
+}
+counter
+
+
+counter<-0;
+for (i in 1:length(logicalVector)){
+  if(logicalVector[i]==T){
+    counter <- counter+1;
+  }
+}
+counter
+
+x <- 100
+if((x%%2)==0){
+  print(x, " là số chẵn");
+}else{
+  print(x, " là số lẻ")
+}
+
+
+
